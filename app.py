ad0415d09a884ddab849083475a5d6d68ea9a126
Erina Kim <erinakim@Erinas-MacBook-Air.local>
2024-06-30 14:38:59 -0700

    added bjoern

--- app.py
+++ app.py
@@ -129,5 +129,6 @@ def ask_question():
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import bjoern
+    bjoern.run(app, "127.0.0.1", 5000)
--- run_bjoern.py
+++
@@ -1,6 +0,0 @@
-import bjoern
-from app import app
-
-# Bind to the PORT environment variable for Heroku
-import os
-port = int(os.environ.get('PORT', 5000))
